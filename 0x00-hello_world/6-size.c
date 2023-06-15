9f08378ae122eb9255193648653e40e0387779bf
HasanElakad <hasanosman2005@gmail.com>
2023-06-15 11:28:04 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  main - this program prints all sizez
+ *
+ *  Return: 0 (successs)
+ */
+
+int mani(void);
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a flaot: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
